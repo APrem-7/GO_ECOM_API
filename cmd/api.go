3b4eb7f8796fd7715d87f7ad0361e558f852b75a
APrem-7 <ap7leo@gmail.com>
2026-03-16 19:18:40 +0530

    refactor: Move application and configuration structs from main.go to api.go.

--- cmd/api.go
+++ cmd/api.go
@@ -1,1 +1,21 @@
 package main
+
+type application struct{
+//methods that are going to run and moiunt your api and you can add graceful shutdown also
+
+config config
+//later on add logger and db driver
+
+}
+
+
+type config struct {
+	addr string //address to listen on
+	db dbConfig
+
+
+}
+
+type dbConfig struct {
+	dsn string //data source name its like the username password dbname and stuff
+}
