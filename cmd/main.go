a55c913e81f05144b8b2c4bddcd7e29d0e3e27f5
APrem-7 <ap7leo@gmail.com>
2026-03-16 19:17:31 +0530

    feat: introduce `application`, `config`, and `dbConfig` structs for application and database configuration.

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
 package main
+
+type application struct{
+//methods that are going to run and moiunt your api and you can add graceful shutdown also
+
+config config
+
+}
+
+
+type config struct {
+	addr string //address to listen on
+	db dbConfig
+
+
+}
+
+type dbConfig struct {
+	dsn string //data source name its like the username password dbname and stuff
+}
