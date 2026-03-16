4b9f79cf80c2abb2bd720597474d3703d99252f7
APrem-7 <ap7leo@gmail.com>
2026-03-16 19:36:11 +0530

    feat: Add `mount` method to `application` struct for initializing Chi router with middleware and a basic GET route.

--- cmd/api.go
+++ cmd/api.go
@@ -1,19 +1,44 @@
 package main
 
-type application struct{
-//methods that are going to run and moiunt your api and you can add graceful shutdown also
+import (
+	"net/http"
+	"time"
 
-config config
-//later on add logger and db driver
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type application struct {
+	//methods that are going to run and moiunt your api and you can add graceful shutdown also
+
+	config config
+	//later on add logger and db driver
 
 }
 
+// mount mounts the routes to the router
+func (app *application) mount() http.Handler {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hi"))
+	})
+}
 
 type config struct {
 	addr string //address to listen on
-	db dbConfig
-
-
+	db   dbConfig
 }
 
 type dbConfig struct {
