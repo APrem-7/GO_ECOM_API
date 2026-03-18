290a5559bd245afafaf6cefaa66c997f79e4ef1c
APrem-7 <ap7leo@gmail.com>
2026-03-19 00:35:47 +0530

    feat: Initialize product service with a ListProduct method and configure sqlc for database code generation.

--- internal/products/services.go
+++ internal/products/services.go
@@ -1,5 +1,21 @@
 package products
 
+import "context"
+
 type Service interface {
 	//define the listProductsService
+	ListProduct(ctx context.Context) (error)
+
+}
+
+type svc struct {
+
+}
+
+func NewService() Service {
+	return &svc{}
+}
+
+func  (s* svc) ListProduct (ctx context.Context) error {
+	return nil
 }
