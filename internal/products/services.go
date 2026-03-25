53a776bcb71c049ad20ee763475478a1e3935b55
APrem-7 <ap7leo@gmail.com>
2026-03-26 02:14:13 +0530

    feat(products): implement product listing with database integration
    - Change ListProduct service method to return products from database
    - Update service constructor to accept repository dependency
    - Replace mock implementation with actual database call

--- internal/products/services.go
+++ internal/products/services.go
@@ -1,21 +1,25 @@
 package products
 
-import "context"
+import (
+	"context"
+
+	repo "github.com/APrem-7/GO_ECOM_API/internal/adapters/postgres/sqlc"
+)
 
 type Service interface {
 	//define the listProductsService
-	ListProduct(ctx context.Context) (error)
-
+	ListProduct(ctx context.Context) ([]repo.Product, error)
 }
 
 type svc struct {
-
+	repo repo.Querier
 }
 
-func NewService() Service {
-	return &svc{}
+func NewService(q repo.Querier) Service {
+	return &svc{repo: q}
 }
 
-func  (s* svc) ListProduct (ctx context.Context) error {
-	return nil
+func (s *svc) ListProduct(ctx context.Context) ([]repo.Product, error) {
+	return s.repo.ListProducts(ctx)
+
 }
