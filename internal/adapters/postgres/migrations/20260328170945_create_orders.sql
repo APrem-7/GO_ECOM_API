7687fabf4cfb80423f1b63dbe154a3d1fb39e309
APrem-7 <ap7leo@gmail.com>
2026-03-28 22:56:37 +0530

    feat(postgres): add orders and order_items tables migration

---
+++ internal/adapters/postgres/migrations/20260328170945_create_orders.sql
@@ -0,0 +1,29 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS orders (
+    id BIGSERIAL PRIMARY KEY,
+    customer_id BIGINT NOT NULL,
+    order_status TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+
+);
+
+CREATE TABLE IF NOT EXISTS order_items (
+    id BIGSERIAL PRIMARY KEY,
+    order_id BIGINT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    quantity INTEGER NOT NULL DEFAULT 1,
+    price_in_centers INTEGER NOT NULL CHECK (price_in_centers>=0),
+    product_id BIGINT NOT NULL,
+    CONSTRAINT fk_order FOREIGN_KEY (order_id) REFERENCES orders(id)
+
+
+
+    
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS orders;
+
