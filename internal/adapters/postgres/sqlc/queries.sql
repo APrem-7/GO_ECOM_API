654a3a636f66be272bd5c4286637961bf7edba3d
APrem-7 <ap7leo@gmail.com>
2026-03-29 02:12:11 +0530

    feat(postgres): add PostOrder query for inserting order items
    Add SQL query to insert a new order item into the order_items table and return the created record. This enables the order placement functionality in the PostgreSQL adapter.

--- internal/adapters/postgres/sqlc/queries.sql
+++ internal/adapters/postgres/sqlc/queries.sql
@@ -3,3 +3,6 @@ SELECT * FROM products;
 
 -- name: GetProductByID :one
 SELECT * FROM products WHERE id=$1;
+
+-- name: PostOrder :one
+INSERT INTO order_items(order_id, quantity,price_in_centers,product_id) VALUES($1, $2, $3, $4) RETURNING *;
