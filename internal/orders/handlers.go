4bb460abfa0e0dbbe7fc31c5b6f81732c7fe6110
APrem-7 <ap7leo@gmail.com>
2026-03-29 01:22:38 +0530

    feat(orders): add handler struct and constructor

--- internal/orders/handlers.go
+++ internal/orders/handlers.go
@@ -1,2 +1,20 @@
 package orders
 
+import (
+	"net/http"
+)
+
+type handler struct {
+	service Service
+}
+
+func NewHandler(service Service) *handler {
+	return &handler{
+		service: service,
+	}
+
+}
+
+func (h *handler) PostOrder(w http.ResponseWriter, r *http.Request) {
+	
+}
