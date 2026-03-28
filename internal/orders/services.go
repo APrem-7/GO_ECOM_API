92487ad32c67422a0004c19870dbfcf12f968046
APrem-7 <ap7leo@gmail.com>
2026-03-29 00:51:46 +0530

    feat(orders): add service interface and implementation

---
+++ internal/orders/services.go
@@ -0,0 +1,20 @@
+package orders
+
+import (
+	"context"
+
+	repo "github.com/APrem-7/GO_ECOM_API/internal/adapters/postgres/sqlc"
+)
+
+type Service interface {
+	//define the orders like list order and post order services
+	PostOrders(ctx context.Context, arg repo.CreateOrderParams) (repo.Order, error)
+}
+
+type svc struct {
+	repo repo.Querier
+}
+
+func NewService(q repo.Querier) Service {
+	return &svc{repo: q}
+}
