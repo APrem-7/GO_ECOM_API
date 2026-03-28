73b7b3f73fcf7d75f8984c67bb581fad95d59065
APrem-7 <ap7leo@gmail.com>
2026-03-29 01:03:24 +0530

    refactor: rename PostOrders parameter for clarity

--- internal/orders/services.go
+++ internal/orders/services.go
@@ -8,7 +8,7 @@ import (
 
 type Service interface {
 	//define the orders like list order and post order services
-	PostOrders(ctx context.Context, arg repo.CreateOrderParams) (repo.Order, error)
+	PostOrders(ctx context.Context, tempOrderArg CreateOrderParams) (repo.Order, error)
 }
 
 type svc struct {
