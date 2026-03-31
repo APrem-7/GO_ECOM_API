723eb1572e73a8321038ec3e88c49941926a8fc7
APrem-7 <ap7leo@gmail.com>
2026-03-31 23:11:37 +0530

    feat(orders): implement JSON parsing in PostOrder handler

--- internal/orders/handlers.go
+++ internal/orders/handlers.go
@@ -1,7 +1,10 @@
 package orders
 
 import (
+	"log"
 	"net/http"
+
+	"github.com/APrem-7/GO_ECOM_API/internal/json"
 )
 
 type handler struct {
@@ -16,5 +19,11 @@ func NewHandler(service Service) *handler {
 }
 
 func (h *handler) PostOrder(w http.ResponseWriter, r *http.Request) {
-	
+	var tempOrder CreateOrderParams
+	if err := json.ReadJSON(r, &tempOrder); err != nil {
+		log.Println("error")
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
 }
