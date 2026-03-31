8d7fa9e85c09f7164ea34c8d3f859c25e6b42715
APrem-7 <ap7leo@gmail.com>
2026-03-31 20:57:24 +0530

    fix(orders): add validation for order creation parameters
    Validate customerID and items array in PostOrders service method to prevent invalid orders from being processed.

--- internal/orders/services.go
+++ internal/orders/services.go
@@ -2,6 +2,7 @@ package orders
 
 import (
 	"context"
+	"fmt"
 
 	repo "github.com/APrem-7/GO_ECOM_API/internal/adapters/postgres/sqlc"
 )
@@ -17,4 +18,18 @@ type svc struct {
 
 func NewService(q repo.Querier) Service {
 	return &svc{repo: q}
+}
+
+func (s *svc) PostOrders(ctx context.Context, tempOrder CreateOrderParams) (repo.Order, error) {
+
+	//validate the payload
+	if tempOrder.CustomerID == 0 {
+		return repo.Order{}, fmt.Errorf("The customerID has to be valid")
+	}
+	if len(tempOrder.Items) == 0 {
+		return repo.Order{}, fmt.Errorf("At least one item is required")
+	}
+	//create an Order
+	//look for the product if exists
+	//create order item
 }
